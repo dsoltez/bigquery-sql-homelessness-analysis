f34f1ab79c0282306cf1a28c935687767780c4b2
Drake Soltez <dsoltez13@gmail.com>
2025-10-24 18:46:25 -0400

    Delete top_50_states_2018.sql

--- top_50_states_2018.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  CoC_Name
-  Sheltered_SH_Homeless 
-FROM `merit-america-data-project-dss.Exploration_Project.homelessness` 
-WHERE
-Count_Year = 2018
-AND Sheltered_SH_Homeless > 0
-ORDER BY
-  CoC_Name;
